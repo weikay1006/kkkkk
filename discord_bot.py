26ed0577ddc51b10afaa24fa24e52e5c1a8e1bbe
weikay1006 <86037460+weikay1006@users.noreply.github.com>
2021-08-09 16:50:40 +0800

    Update discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -1,32 +1,27 @@
 #導入Discord.py
-import asyncio
 import time
 import discord
-import random, os, asyncio
-import keep_alive
-from discord.ext import commands
-import json
-
-#bot是我們與Discord連結的橋樑
-bot = discord.bot()
+import random
+#client是我們與Discord連結的橋樑
+client = discord.Client()
 
 
 
 #調用event函式庫
-@bot.event
+@client.event
 #當機器人完成啟動時
 async def on_ready():
-    print('目前登入身份：',bot.user)
+    print('目前登入身份：',client.user)
     print('目前運作正常 如有問題請找39.peko')
     game = discord.Game('努力學習py中')
     #discord.Status.<狀態>，可以是online,offline,idle,dnd,invisible
-    await bot.change_presence(status=discord.Status.dnd, activity=game)
+    await client.change_presence(status=discord.Status.dnd, activity=game)
 
 
 
 
 
-@bot.event
+@client.event
 #當有訊息時
 async def on_message(message):
     #如果以「說」開頭
@@ -47,11 +42,11 @@ async def on_message(message):
         else:
             game = discord.Game(tmp[1])
             #discord.Status.<狀態>，可以是online,offline,idle,dnd,invisible
-            await bot.change_presence(status=discord.Status.idle, activity=game)
+            await client.change_presence(status=discord.Status.idle, activity=game)
     
     if message.content == '所在群組':
         #獲取當前所在群組(極限150個，預設為100個)，並用flatten將它全部移到guilds這個list裡面
-        guilds = await bot.fetch_guilds(limit=150).flatten()
+        guilds = await client.fetch_guilds(limit=150).flatten()
         #遍尋 guilds
         for i in guilds:
             #由於我們只要 guilds 的name 就好，當然也可以獲取 id~
@@ -64,10 +59,10 @@ async def on_message(message):
         await message.delete()
 
 
+
         
                 
         
         
-if __name__ == '__main__':
-    keep_alive.keep_alive()
-    bot.run('token') #TOKEN 在剛剛 Discord Developer 那邊「BOT」頁面裡面
+
+client.run('Token') #TOKEN 在剛剛 Discord Developer 那邊「BOT」頁面裡面
